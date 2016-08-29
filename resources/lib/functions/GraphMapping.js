35efb76512227a227889b2e2d3317da9f18c2640
mchazalviel <maxime.chazalviel@toulouse.inra.fr>
2016-08-29 08:52:41 +0200

    HotFix

--- resources/lib/functions/GraphMapping.js
+++ resources/lib/functions/GraphMapping.js
@@ -170,7 +170,7 @@ metExploreD3.GraphMapping = {
 	* This function will assignmapping value to each nodes in datas
 	*/
 	mapNodeData: function(mapping, lines) {
-
+		console.log(lines);
 		var session = _metExploreViz.getSessionById('viz');
 		var force = session.getForce();
 		force.stop(); 
@@ -1600,55 +1600,55 @@ metExploreD3.GraphMapping = {
 				        //     aData.color=scale(aData.z);
 				        // });
 
-				        d3.select("#viz").select("#D3viz").select("#graphComponent").selectAll("g.node")
-							.each(function(node){
-								if (node.getMappingDatasLength()!=0)
-								{
-									var maps = node.getMappingDatas();
-									var dataCond1 = -Math.abs(parseInt(maps[0].getMapValue()));
-									var dataCond2 = Math.abs(parseInt(maps[1].getMapValue()));
+// 				        d3.select("#viz").select("#D3viz").select("#graphComponent").selectAll("g.node")
+// 							.each(function(node){
+// 								if (node.getMappingDatasLength()!=0)
+// 								{
+// 									var maps = node.getMappingDatas();
+// 									var dataCond1 = -Math.abs(parseInt(maps[0].getMapValue()));
+// 									var dataCond2 = Math.abs(parseInt(maps[1].getMapValue()));
 
-							        var conditions2=
-							        [
-							            {
-							                name: maps[0].getConditionName(),
-							                data: 500
-							            }, {
-							                name: maps[1].getConditionName(),
-							                data: 500
-							            }
-							        ];
-var categories2 = [
-            'Alanine and aspartate metabolism',
-            'Alkaloid synthesis'
-];
-        var conditions2=
-        [
-            {
-                name: '3j',
-                data: [-5, -54]
-            }, {
-                name: '30j',
-                data: [75, 20]
-            }
-        ];
-        var dataChart2 = {categories:categories2, conditions:conditions2};
-         // var element3 = new MetXCompareBar(dataChart2, 1300, 1000, "xaxis", "yaxis", "title");
-					        		var minDim=1000;
-	        						// var dataChart2 = {categories:[node.getName()], conditions:conditions2};
-	        						var chartSvg = d3.select(this).append("svg")
-										.attr("viewBox",function(d) {return "0 0 "+minDim+" "+minDim;})
-										.attr("width", minDim *8/10 + "px")
-										.attr("height", minDim *8/10+ "px")
-										// .attr("x", (-minDim/2)+(minDim*1/10))
-										// .attr("y", (-minDim/2)+(minDim*1/10))
-					        		var compareChart = new MetXCompareBar(dataChart2, 1000, 200, "xaxis", "yaxis", maps[0].getMappingName() +" analysis");
+// 							        var conditions2=
+// 							        [
+// 							            {
+// 							                name: maps[0].getConditionName(),
+// 							                data: 500
+// 							            }, {
+// 							                name: maps[1].getConditionName(),
+// 							                data: 500
+// 							            }
+// 							        ];
+// var categories2 = [
+//             'Alanine and aspartate metabolism',
+//             'Alkaloid synthesis'
+// ];
+//         var conditions2=
+//         [
+//             {
+//                 name: '3j',
+//                 data: [-5, -54]
+//             }, {
+//                 name: '30j',
+//                 data: [75, 20]
+//             }
+//         ];
+//         var dataChart2 = {categories:categories2, conditions:conditions2};
+//          // var element3 = new MetXCompareBar(dataChart2, 1300, 1000, "xaxis", "yaxis", "title");
+// 					        		var minDim=1000;
+// 	        						// var dataChart2 = {categories:[node.getName()], conditions:conditions2};
+// 	        						var chartSvg = d3.select(this).append("svg")
+// 										.attr("viewBox",function(d) {return "0 0 "+minDim+" "+minDim;})
+// 										.attr("width", minDim *8/10 + "px")
+// 										.attr("height", minDim *8/10+ "px")
+// 										// .attr("x", (-minDim/2)+(minDim*1/10))
+// 										// .attr("y", (-minDim/2)+(minDim*1/10))
+// 					        		var compareChart = new MetXCompareBar(dataChart2, 1000, 200, "xaxis", "yaxis", maps[0].getMappingName() +" analysis");
 					        		
-										chartSvg.html(d3.select(compareChart).select('svg').node().outerHTML)
+// 										chartSvg.html(d3.select(compareChart).select('svg').node().outerHTML)
 										
-										// .attr("width", "100%").attr("height", "100%");
-								}
-							});
+// 										// .attr("width", "100%").attr("height", "100%");
+// 								}
+// 							});
 						// var array = [];
 						// d3.select(compareChart).select('svg').selectAll('.highcharts-series').selectAll('rect').each(function(){array.push(this.height.animVal.value)});
 						
