1703d9a1e00b55c1a42630465376772097a3c4d2
mchazalviel <root@vm-metexplore>
2016-11-18 09:28:50 +0100

    improve production script

--- scripts/viztoprod.sh
+++ scripts/viztoprod.sh
@@ -1,24 +1,32 @@
 
 #!/bin/sh
-
 # Go to MetExploreViz folder
-cd /var/www/MetExploreViz/
-# Build ExtJS
-sudo /root/bin/Sencha/Cmd/6.1.3.42/sencha app build  
 
+if [ $# -ne 1 ] # s'il n'y a pas 1 paramètres 
+then
+         echo 'Parameter is required : \n\t1: sencha cmd path'
+else
+	arg=$1
+        
+	cd ../
+	# Build ExtJS
+	sudo $arg app build  
 
-cd /var/www/scripts/
-# Link to the file which contain metExploreViz version 
-appJS="/var/www/MetExploreViz/build/production/metExploreViz/app.json"
+	cd ./scripts/
+	# Link to the file which contain metExploreViz version 
+	appJS="/var/www/MetExploreViz/build/production/metExploreViz/app.json"
 
-# Parse JSON to get version
-sub='\["version"]'
-lineversion=$(./JSON.sh -l < $appJS| egrep $sub)
-version=${lineversion#$sub}
-newversion=$(echo $version | sed s/\"//g)
-echo $newversion
+	# Parse JSON to get version
+	sub='\["version"]'
+	lineversion=$(./JSON.sh -l < $appJS| egrep $sub)
+	version=${lineversion#$sub}
+	newversion=$(echo $version | sed s/\"//g)
+	echo $newversion
 
-# ./licensesScriptMetExploreViz/compil\&licence.sh => add licenses
-# ../MetExploreViz/build/production/metExploreViz/  => MetExploreViz production folder
-sudo ./licenses/compil\&licence.sh ../MetExploreViz/build/production/metExploreViz/ $newversion
+	# ../licenses/compil\&licence.sh => add licenses
+	# ../build/production/metExploreViz/  => MetExploreViz production folder
+	sudo ../licenses/compil\&licence.sh ../build/production/metExploreViz/ $newversion
+
+ 
+fi
 
