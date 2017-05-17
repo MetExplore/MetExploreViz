ea842a3150862dfbdcbed80bf369f7e571a3ad75
maxchaza <maxime.chazalviel@medday-pharma.com>
2017-05-17 18:34:55 +0200

    Add init for frame by element

--- metexploreviz.js
+++ metexploreviz.js
@@ -45,6 +45,50 @@ var MetExploreViz = {
 		iframe.src = source;
 	},
 
+	initFrameInElement:function(elmt){
+		var iframe = document.createElement('iframe');
+		// iframe.rel = "external",
+		iframe.id = "iFrameMetExploreViz",
+		iframe.height = '100%',
+		iframe.width = '100%',
+		iframe.border = 0,
+		iframe.setAttribute("style", "border: none;top: 0; right: 0;bottom: 0; left: 0; width: 100%;height: 100%;");
+		
+		var scripts = document.getElementsByTagName("script");
+		var libSrc;
+		for (var i = 0; i < scripts.length; i++) {
+			if (scripts[i].src.search("metExploreViz/metexploreviz.js") != -1) {
+				libSrc = scripts[i].src;
+			}
+		};
+		var locationSrc = document.location.href;
+
+		var index=0;
+		while (libSrc[index]==locationSrc[index] 
+				&& index != locationSrc.length 
+					&& index != libSrc.length) {
+		    index++;
+		}
+
+		var libSrc = libSrc.substr(index, libSrc.length-1);
+		libSrc = libSrc.split("/metexploreviz.js");
+
+		res = locationSrc.substr(index, locationSrc.length-1);
+
+		res = res.split('/');
+
+		var headString = "";
+
+		for (var i = 0; i < res.length-1; i++) {
+			headString+="../"; 	
+		};
+
+		var source = headString + libSrc[0] + "/index.html";
+
+		elmt.insertBefore(iframe, elmt.firstChild);
+		iframe.src = source;
+	},
+
 	launchMetexploreFunction:function(func) {
         if (typeof metExploreViz !== 'undefined') {
            // the variable is defined
