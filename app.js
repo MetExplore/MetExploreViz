2ecc425c7557663e7a651ec201d6bacb12dfc7dc
maxchaza <maxime.chazalviel@medday-pharma.com>
2017-06-07 14:54:23 +0200

    message #test

--- app.js
+++ app.js
@@ -1,30 +1,3 @@
-// /*
-//  * This file is generated and updated by Sencha Cmd. You can edit this file as
-//  * needed for your application, but these edits will have to be merged by
-//  * Sencha Cmd when upgrading.
-//  */
-// Ext.application({
-//     name: 'metExploreViz',
-
-//     extend: 'metExploreViz.Application',
-
-//     requires: [
-//         'metExploreViz.view.main.Main'
-//     ],
-
-//     // The name of the initial view to create. With the classic toolkit this class
-//     // will gain a "viewport" plugin if it does not extend Ext.Viewport. With the
-//     // modern toolkit, the main view will be added to the Viewport.
-//     //
-//     mainView: 'metExploreViz.view.main.Main'
-	
-//     //-------------------------------------------------------------------------
-//     // Most customizations should be made to metExploreViz.Application. If you need to
-//     // customize this file, doing so below this section reduces the likelihood
-//     // of merge conflicts when upgrading to new versions of Sencha Cmd.
-//     //-------------------------------------------------------------------------
-// });
-
 /**
 * @author MC
 * @description : Interface with other developments
